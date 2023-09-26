b729cb76e88a44d7f62ca97c7753d8cc4078586a
= <=>
2023-09-26 22:00:43 +0530

    Commit : 28/08/2023

--- src/main/java/JavaMethods.java
+++ src/main/java/JavaMethods.java
@@ -1,2 +1,13 @@
 public class JavaMethods {
+
+    public static void main(String[] args) {
+
+        int c = addition(10,20);
+        System.out.println("Addition of the two numbers is : " + c);
+    }
+
+    static int addition(int a, int b){
+        return a + b;
+
+    }
 }
---
+++ src/main/java/owner/PublicConfig.java
@@ -0,0 +1,9 @@
+package owner;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"E:/SDET/CoreJava/src/main/java/owner/Config.properties"})
+public interface PublicConfig extends Config {
+
+
+}
